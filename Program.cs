25f01bdf501931bf534a19a6199245d05d40d095
Attila Tóth <attila.toth10@tesco.com>
2022-10-14 12:21:40 +0200

    read by numbers iter. 1

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace sajat_megoldas
 {
     class Program
     {
+        static List<int> points = new List<int>();
+        static int numberOfRacer;
+
         static void Main(string[] args)
         {
             int startMillis = DateTime.Now.Millisecond;
 
             int[] inputs = Array.ConvertAll(Console.ReadLine().Split(' ', 2), Int32.Parse);
-            int numberOfRacer = inputs[0];
+            numberOfRacer = inputs[0];
             int maxPoints = inputs[1];
 
-            int[] points = Array.ConvertAll(Console.ReadLine().Split(' ', numberOfRacer), Int32.Parse);
             int[] scores = new int[maxPoints + 1];
 
             int place = 1;
-            scores[points[0]] = 1;
+            int point = ReadNextNumber();
+            int lastPoint = point;
+            scores[point] = 1;
 
             Console.WriteLine("{0} {1}", 1, 1);
 
@@ -24,8 +29,7 @@ namespace sajat_megoldas
 
             for (int i = 1; i < numberOfRacer; i++)
             {
-                int lastPoint = points[i - 1];
-                int point = points[i];
+                point = ReadNextNumber();
 
                 if (point < lastPoint)
                 {
@@ -42,10 +46,26 @@ namespace sajat_megoldas
                     }
                 }
                 Console.WriteLine("{0} {1}", place, ++scores[point]);
+                lastPoint = point;
             }
 
             Console.Error.WriteLine("cycle run time: {0}", middleMillis - startMillis);
             Console.Error.WriteLine("full run time: {0}", DateTime.Now.Millisecond - startMillis);
+        }
+
+        static int idx = 0;
+
+        static int ReadNextNumber()
+        {
+            if (points.Count == 0)
+            {
+                string[] split = Console.ReadLine().Split(' ', numberOfRacer);
+                foreach (var s in split)
+                {
+                    points.Add(Int32.Parse(s));
+                }
+            }
+            return points[idx++];
         }
     }
 }
